071088607f9fa52f02999c452e9527b9b3f50888
zhheo <qewan@foxmail.com>
2021-08-16 11:26:57 +0800

    添加横向滚动

--- categoryBar/categoryBar.js
+++ categoryBar/categoryBar.js
@@ -1,4 +1,6 @@
 categoriesBarActive()
+topCategoriesBarScroll()
+
 //分类条
 function categoriesBarActive(){
   var urlinfo = window.location.pathname;
@@ -26,4 +28,18 @@ function categoriesBarActive(){
     }
   }
   
+}
+
+//鼠标控制横向滚动
+function topCategoriesBarScroll(){
+  if (document.getElementById("category-bar-items")){
+    let xscroll = document.getElementById("category-bar-items");
+  xscroll.addEventListener("mousewheel", function (e) {
+    //计算鼠标滚轮滚动的距离
+    let v = -e.wheelDelta / 2;
+    xscroll.scrollLeft += v;
+    //阻止浏览器默认方法
+    e.preventDefault();
+}, false);
+  }
 }
